4ac2111cc3428a05b0a638bf41a99da6bd7f217e
Fahmimenjadihengker <fahmi@students.amikom.ac.id>
2026-09-23 03:06:23 +0700

    fix: resolve test assertion to pass CI checks

--- src/states/authUser/reducer.test.js
+++ src/states/authUser/reducer.test.js
@@ -21,7 +21,7 @@ describe('authUserReducer function', () => {
     const nextState = authUserReducer(initialState, action);
 
     // assert
-    expect(nextState).toEqual('TRIGGER_CI_FAILURE');
+    expect(nextState).toEqual(initialState);
   });
 
   it('should return the authUser when given by SET_AUTH_USER action', () => {
